656fa962a5e3f1a86999a3ecf85965d300a1dcfc
Richard Bromley <bromleyrh@gmail.com>
2021-12-12 14:47:50 -0800

    Fix build files to correct test linking on OS X
    Commit UUID: b165fa44-3733-4f40-8d58-299bf1d307e3

---
+++ trunk/scripts/fix_up_linking_os_x.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+testdir=tests
+
+get_cur_libutil_path()
+{
+	otool -L "$1" | grep libn | cut -d ' ' -f 1 | cut -f 2-
+}
+
+replace_path()
+{
+	install_name_tool -change "$@"
+}
+
+case $1 in
+	"${testdir}"*)
+		curpath=$(get_cur_libutil_path "$1")
+		test -z "$curpath" && exit 0
+		replace_path "$curpath" "$2" "$1";;
+esac
+
+# vi: set noexpandtab sw=4 ts=4:
