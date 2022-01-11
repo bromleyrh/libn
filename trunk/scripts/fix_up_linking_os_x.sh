0fa2c9c1bc1242f6128c102c37c62fc6b1561769
Richard Bromley <bromleyrh@gmail.com>
2022-01-10 23:24:57 -0800

    Fix name of function invoking otool command
    Commit UUID: 2d943f9a-358e-41a1-9e5f-9bd7397ab553

--- trunk/scripts/fix_up_linking_os_x.sh
+++ trunk/scripts/fix_up_linking_os_x.sh
@@ -2,7 +2,7 @@
 
 testdir=tests
 
-get_cur_libutil_path()
+get_cur_libn_path()
 {
 	otool -L "$1" | grep libn | cut -d ' ' -f 1 | cut -f 2-
 }
@@ -14,7 +14,7 @@ replace_path()
 
 case $1 in
 	"${testdir}"*)
-		curpath=$(get_cur_libutil_path "$1")
+		curpath=$(get_cur_libn_path "$1")
 		test -z "$curpath" && exit 0
 		replace_path "$curpath" "$2" "$1";;
 esac
