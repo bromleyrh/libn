5a4a760a286d37db39d71de487e89340c943942a
Richard Bromley <bromleyrh@gmail.com>
2022-02-19 21:41:30 -0800

    Add diagnostic and error output macros to common.h
    Commit UUID: cd7379ff-825b-4bab-9ba0-e356642e89ca

---
+++ trunk/common.h
@@ -0,0 +1,23 @@
+/*
+ * common.h
+ */
+
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#include <stdio.h>
+
+#define stderrchr(c) fputc(c, stderr)
+#define stderrmsg(msg) fputs(msg, stderr)
+#define stderrmsgf(format, ...) fprintf(stderr, format, ##__VA_ARGS__)
+
+#define infochr stderrchr
+#define infomsg stderrmsg
+#define infomsgf stderrmsgf
+
+#define errmsg stderrmsg
+#define errmsgf stderrmsgf
+
+#endif
+
+/* vi: set expandtab sw=4 ts=4: */
